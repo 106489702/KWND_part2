b5b4d38fc027a2e2a85dea33903320c522881fb2
105559662 <105559662@student.swin.edu.au>
2026-05-29 09:23:31 +1000

    Update manage.php

--- project2/manage.php
+++ project2/manage.php
@@ -1,1 +1,41 @@
 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+
+if (!isset($_SESSION["admin"])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php include 'header.inc'; ?>
+<?php include 'nav.inc'; ?>
+
+
+<main>
+    <h1>HR Manager Page</h1>
+
+    <p>You are successfully logged in as admin.</p>
+
+    <p>This page is protected. Only logged-in users can access it.</p>
+
+    <p><a href="logout.php">Logout</a></p>
+</main>
+
+<?php include 'footer.inc'; ?>
+
+</body>
+</html>
