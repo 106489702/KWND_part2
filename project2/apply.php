d502e03e307ef6ba281991240c131d044ef2fdac
NguyenPham214 <105914465@student.swin.edu.au>
2026-05-29 01:08:42 +1000

    Update apply.php

--- project2/apply.php
+++ project2/apply.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Apply for a role at KWND Creative">
-    <meta name="keywords" content="Apply, Job Application, Front-End Developer, Designer, Creative Agency, KWND Creative">
-    <meta name="author" content="G06">
-    <title>Apply | KWND Creative</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
 <?php include 'header.inc'; ?> 
 
     <main>
@@ -21,7 +9,8 @@
             </p>
         </section>
 
-        <form action="https://mercury.swin.edu.au/it000000/formtest.php" method="post" id="applicationForm">
+        <form action="process_eoi.php" method="post" id="applicationForm" novalidate>
+        <!-- lets the validation only happen in process_eoi.process -->
 
             <div class="formGrid">
 
@@ -108,12 +97,12 @@
                 <!-- Contact -->
                 <div class="formRow">
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required>
+                    <input type="text" id="email" name="email" required>
                 </div>
 
                 <div class="formRow">
                     <label for="phone">Phone Number</label>
-                    <input type="tel" id="phone" name="phone" required
+                    <input type="text" id="phone" name="phone" required
                         pattern="[0-9]{8,12}" maxlength="12">
                 </div>
 
