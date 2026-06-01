5ebdd1d38ed9e29fa072f5e759d4213ceba054c0
Khuong Duy Phan <105559662@student.swin.edu.au>
2026-06-01 17:11:53 +1000

    update sql

--- project2/kwnd_db.sql
+++ project2/kwnd_db.sql
@@ -152,3 +152,6 @@ CREATE TABLE `eoi` (
   `Status`      ENUM('New','Current','Final') NOT NULL DEFAULT 'New',
   PRIMARY KEY (`EOINumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
